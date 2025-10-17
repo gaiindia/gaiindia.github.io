7b188c28b5d14aff628140848e382e11a53e5208
gaiindia <153261957+gaiindia@users.noreply.github.com>
2025-10-17 18:07:13 +0530

    contact form updated

---
+++ footer.php
@@ -0,0 +1,197 @@
+<!--Start footer area Style4-->
+<footer class="footer-area style4">
+    <div class="container">
+        <div class="row">
+            <!--Start single footer widget-->
+            <!-- <div class="col-xl-3 col-lg-5 col-md-6 col-sm-12">
+                        <div class="single-footer-widget s4">
+                            <div class="title-style2">
+                                <h3>About US</h3>
+                            </div>
+                            <p>Sureologics Kitchen Solutions Is One Of The Leading Company In Pune. We Wholesale Trader of the Commercial Kitchen Equipment's.</p>
+                            
+                        </div>
+                    </div> -->
+            <!--End single footer widget-->
+            <!--Start single footer widget-->
+            <div class="col-xl-2 col-lg-5 col-md-6 col-sm-12">
+                <div class="single-footer-widget s4">
+                    <div class="title-style2">
+                        <h3>Usefull Links</h3>
+                    </div>
+                    <div class="usefull-links">
+                        <ul class="float-left">
+                            <li><a href="index.html">Home</a></li>
+                            <li><a href="about-us.html">About Us</a></li>
+                            <!-- <li><a href="#">Product</a></li> -->
+                            <li><a href="gallery.html">Gallery</a></li>
+                            <li><a href="clients.html">Clients</a></li>
+                            <li><a href="contact-us.html">Contact Us</a></li>
+                        </ul>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-5 col-lg-5 col-md-6 col-sm-12">
+                <div class="single-footer-widget s4">
+                    <div class="title-style2">
+                        <h3> Contact Details </h3>
+                    </div>
+                    <div class="usefull-links removePlus">
+                        <ul class="float-left">
+                            <li>
+                                <a href="#">
+                                    <i class="fa fa-user" aria-hidden="true"> </i>GAI INDIA HOSPITALITY SOLUTIONS PVT.
+                                    LTD.
+                                </a>
+                            </li>
+                            <hr>
+                            <li><a href="tel:+917588800333"><i class="fa fa-phone" aria-hidden="true"> </i> +91
+                                    7588800333</a></li>
+                            <li><a href="tel:+917447444327"><i class="fa fa-phone" aria-hidden="true"> </i> +91
+                                    7447444327</a></li>
+                            <hr>
+
+                            <li><a href="mailto:sales@gaiindia.com.in"><i class="fa fa-envelope" aria-hidden="true">
+                                    </i> sales@gaiindia.com.in </a></li>
+                            <hr>
+                            <li style="color:#d9dade"><i class="fa fa-map-marker" aria-hidden="true"> </i>
+                                <strong>GAI INDIA HOSPITALITY SOLUTIONS PVT. LTD. </strong><br>
+                                Sector 07, Plot No.178/2 & 3, Pcntda, Pimpri<br> Chinchwad, Bhosari, Pune - 411026 ,
+                                MH,India
+                            </li>
+
+                        </ul>
+                        <ul class="forfootersocal">
+                            <li>
+                                <a href="tel:+917588800333">
+                                    <i class="fa fa-phone" aria-hidden="true"> </i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://wa.link/385d89">
+                                    <i class="fa fa-whatsapp" aria-hidden="true"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="fa fa-linkedin" aria-hidden="true"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="fa fa-instagram" aria-hidden="true"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="fa fa-youtube" aria-hidden="true"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <!--End single footer widget-->
+            <!--Start single footer widget-->
+            <div class="col-xl-5 col-lg-3 col-md-12 col-sm-12">
+                <div class="single-footer-widget pdtop50-s4">
+                    <div class="title-style2">
+                        <h3>Location Map</h3>
+                    </div>
+                    <div class="subscribe-box">
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3780.476285551568!2d73.8374683!3d18.6426113!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2b9be47053f1b%3A0x18bbf8ae0d9e38f5!2sGAI%20INDIA%20COMMERCIAL%20KITCHEN%20EQUIPMENTS!5e0!3m2!1sen!2sin!4v1726040706551!5m2!1sen!2sin"
+                            width="100%" height="250" style="border:0;" allowfullscreen="" loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+            </div>
+            <!--End single footer widget-->
+        </div>
+    </div>
+</footer>
+<!--End footer area style4-->
+<section class="footer-bottom-area style3">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-6 col-12 text-center">
+                <span class="copyright-text ">Copyright © 2024 <a href="index.html" class="text-primary">
+                        GAI India Hospitality Solutions</a> All Rights Reserved.</span>
+            </div>
+
+            <div class="col-md-6 col-12 text-center">
+                <span class="copyright-text">Designed &amp; Developed By <a href="https://www.weblinkservices.net"
+                        class="text-primary" target="_blank"> <img src="images/wlspl_logo.png " width="130 "></a></span>
+            </div>
+
+        </div>
+    </div>
+</section>
+<!--End footer bottom area-->
+
+</div>
+
+
+<div class="scroll-to-top scroll-to-target" data-target="html"><span class="fa fa-angle-up"></span></div>
+
+<a href="https://wa.link/385d89" target="_blank"><img class="circle1 pulse" src="images/whatsapp.png" alt="whatsapp"
+        width="30"></a>
+<a href="tel:+917588800333"><img class="circle2 pulse" src="images/c.png" width="30"></a>
+
+
+
+<script src="js/jquery.js"></script>
+<script src="js/appear.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/bootstrap-select.min.js"></script>
+<script src="js/isotope.js"></script>
+<script src="js/jquery.bootstrap-touchspin.js"></script>
+<script src="js/jquery.countTo.js"></script>
+<script src="js/jquery.easing.min.js"></script>
+<script src="js/jquery.enllax.min.js"></script>
+<script src="js/jquery.fancybox.js"></script>
+<script src="js/jquery.mixitup.min.js"></script>
+<script src="js/jquery.paroller.min.js"></script>
+<script src="js/owl.js"></script>
+<script src="js/validation.js"></script>
+<script src="js/wow.js"></script>
+
+<!---
+<script src="js/gmaps.js"></script>
+<script src="http://maps.google.com/maps/api/js?key=AIzaSyB2uu6KHbLc_y7fyAVA4dpqSVM4w9ZnnUw"></script>
+<script src="js/mapapi.js"></script> 
+--->
+<script src="js/map-helper.js"></script>
+
+<script src="assets/language-switcher/jquery.polyglot.language.switcher.js"></script>
+<script src="assets/timepicker/timePicker.js"></script>
+<script src="assets/html5lightbox/html5lightbox.js"></script>
+
+<!--Revolution Slider-->
+<script src="js/custom.js"></script>
+<script src="plugins/revolution/js/jquery.themepunch.revolution.min.js"></script>
+<script src="plugins/revolution/js/jquery.themepunch.tools.min.js"></script>
+<script src="plugins/revolution/js/extensions/revolution.extension.actions.min.js"></script>
+<script src="plugins/revolution/js/extensions/revolution.extension.carousel.min.js"></script>
+<script src="plugins/revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
+<script src="plugins/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+<script src="plugins/revolution/js/extensions/revolution.extension.migration.min.js"></script>
+<script src="plugins/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+<script src="plugins/revolution/js/extensions/revolution.extension.parallax.min.js"></script>
+<script src="plugins/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+<script src="plugins/revolution/js/extensions/revolution.extension.video.min.js"></script>
+<script src="js/main-slider-script.js"></script>
+<script src="js/lightbox.js"></script>
+<script src="js/script.js"></script>
+<!-- thm custom script -->
+
+
+
+</body>
+
+
+<!-- Mirrored from st.ourhtmldemo.com/new/Crystalo/index-2.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 02 Aug 2024 04:31:55 GMT -->
+
+</html>
